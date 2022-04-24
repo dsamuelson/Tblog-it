07a3977349ef2b090c486fc0e18c6a40032b6aa5
David Samuelson <linksn.1fan@gmail.com>
2022-04-24 12:26:59 -0600

    fixed create post issues

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,8 +1,8 @@
 async function newFormHandler(event) {
     event.preventDefault();
 
-    const title = dcoument.querySelector('input[name="post-title"]').value;
-    const post_content = document.querySelector('textarea[name="post-content]').value;
+    const title = document.querySelector('input[name="post-title"]').value;
+    const post_content = document.querySelector('textarea[name="post-content"]').value.trim();
 
     const response = await fetch(`/api/posts`, {
         method: 'POST',
